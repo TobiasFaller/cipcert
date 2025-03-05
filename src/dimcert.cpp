7ea61c9afa7f5c5843d898133e3c9ee5e3e31112
froleyks <nfroleyks@gmail.com>
2025-03-05 14:23:01 +0100

    rewrote indexing

--- src/dimcert.cpp
+++ src/dimcert.cpp
@@ -39,82 +39,72 @@ auto param(int argc, char *argv[]) {
   return std::tuple{argv[1], argv[2], checks};
 }
 
-std::vector<std::pair<int64_t, int64_t>> index_consecutively(Dimspec &witness,
-                                                             Dimspec &model) {
-  std::vector<std::pair<int64_t, int64_t>> shared;
-  int64_t new_size { 0 };
-  int64_t model_size { model.size() };
-  int64_t model_shift { 0 };
-  int64_t model_next_shift { 0 };
-  int64_t witness_size { witness.size() };
-  int64_t witness_shift { 0 };
-  int64_t witness_next_shift { 0 };
-  if (witness.simulation.empty()) {
+
+std::pair<std::vector<int64_t>, std::vector<int64_t>> index_consecutively(Dimspec &witness,
+                                                                          Dimspec &model) {
+  auto sign = [](int64_t l) { return l < 0 ? -1 : 1; };
+  const int64_t witness_size { witness.size() };
+  const int64_t model_size { model.size() };
+  const int64_t new_size { witness.size() + model.size() };
+  const int64_t max_shared_size { std::min(witness_size, model_size) };
+
+  const int64_t witness_shift { 0 };
+  const int64_t witness_next_shift { model_size };
+  const int64_t model_shift { witness_size };
+  const int64_t model_next_shift { 2 * witness_size };
+
+  std::vector<std::pair<int64_t, int64_t>> shared { witness.simulation };
+
+  if (shared.empty()) {
     MSG << "No witness mapping found, using default\n";
-    assert (witness_size >= model_size);
-    // Remap variables [model|mnext] and [witness|wnext] to [witness|wnext].
-    // Note: The number of variables in the model might be less than the witnesse's.
-    // => We might need to shift the model's next variables to behind witness.
-    new_size = witness_size; // doesn't include next variables
-    model_shift = 0;
-    model_next_shift = (witness_size - model_size);
-    witness_shift = 0;
-    witness_next_shift = 0;
-    shared.reserve(2 * model_size);
-    for (int64_t m { 1 }; m < model_size + 1; ++m) {
-      shared.push_back({ m , m });
-      shared.push_back({ m + model_size, m + model_size + model_next_shift });
-    }
-  } else {
-    // Remap variables [model|mnext] and [witness|wnext] to [witness|model|wnext|mnext].
-    // Note: The shared variables are mapped according to the user-provided data.
-    new_size = model_size + witness_size; // doesn't include next variables
-    model_shift = witness_size;
-    model_next_shift = 2 * witness_size;
-    witness_shift = 0;
-    witness_next_shift = model_size;
-    shared.reserve(2 * witness.simulation.size());
-    for (auto [w, m] : witness.simulation) {
-      shared.push_back({ m, w });
-      shared.push_back({ m + model_size, w + witness_size + witness_next_shift });
+    shared.reserve(max_shared_size);
+    for (int64_t l { 1 }; l < max_shared_size + 1; ++l) {
+      shared.push_back({ l , l });
     }
   }
-  // The size of the model might be smaller than the witness.
-  // If we are re-mapping with a user-provided mapping then the wittness size changes too.
-  // => We need to re-map the model current and next variables.
-  if (model_size != new_size) {
-    std::vector<int64_t> model_map (2 * model_size);
-    std::iota(std::begin(model_map), std::begin(model_map) + model_size, 1 + model_shift);
-    std::iota(std::begin(model_map) + model_size, std::end(model_map),   1 + model_size + model_next_shift);
-    for (auto& [m, w] : shared)
-      model_map[m - 1] = w;
-    for (auto &cnf : {&model.initial, &model.universal, &model.goal, &model.transition}) {
-      cnf->n = new_size;
-      for (auto &clause : cnf->clauses)
-        for (auto &l : clause) {
-          l = model_map[std::abs(l) - 1] * ((l < 0) ? -1 : 1);
-        }
-    }
+
+  std::vector<int64_t> witness_map(2 * witness_size + 1);
+  for (int64_t i { 1 }; i < witness_map.size(); ++i)
+    witness_map[i] = i + (i - 1 < witness_size ? witness_shift : witness_next_shift);
+  std::vector<int64_t> model_map(2 * model_size + 1);
+  for (size_t i { 1 }; i < model_map.size(); ++i)
+    model_map[i] = i + (i - 1 < model_size ? model_shift : model_next_shift);
+
+  for(auto [w, m] : shared) {
+    model_map[m] = witness_map[w];
+    model_map[m + model_size] = witness_map[w + witness_size];
   }
-  // If we are re-mapping with a user-provided mapping then the wittness size changes.
-  // => We need to re-map the witness next variables.
-  if (witness_size != new_size) {
-    for (auto &cnf : {&witness.initial, &witness.universal, &witness.goal, &witness.transition}) {
-      cnf->n = new_size;
-      for (auto &clause : cnf->clauses)
-        for (auto &l : clause)
-          if (std::abs(l) <= witness_size) {
-            l += witness_shift * ((l < 0) ? -1 : 1);
-          } else {
-            l += witness_next_shift * ((l < 0) ? -1 : 1);
-          }
-    }
+
+  for (auto &cnf : {&witness.initial, &witness.universal, &witness.goal, &witness.transition}) {
+    cnf->n = new_size;
+    for (auto &clause : cnf->clauses)
+      for (auto &l : clause)
+        l = witness_map[std::abs(l)] * sign(l);
   }
-  return shared;
+
+  for (auto &cnf : {&model.initial, &model.universal, &model.goal, &model.transition}) {
+    cnf->n = new_size;
+    for (auto &clause : cnf->clauses)
+      for (auto &l : clause)
+        l = model_map[std::abs(l)] * sign(l);
+  }
+
+  std::vector<int64_t> extension, next_extension;
+  std::vector<bool> is_shared(witness_size + 1);
+  for (auto [w, _] : shared)
+    is_shared[w] = true;
+  for (int i = 1; i < witness_size + 1; ++i){
+    if (is_shared[i])
+      continue;
+    extension.push_back(witness_map[i]);
+    next_extension.push_back(witness_map[i + witness_size]);
+  }
+
+  return { extension, next_extension };
 }
 
 void reset(const char *path, const Dimspec &witness, const Dimspec &model,
-           const std::vector<std::pair<int64_t, int64_t>> &shared) {
+           const std::vector<int64_t> &extension) {
   // check that forall(s) i ^ u => exist(s'\s) i' ^ u'
   QCir check {
     qneg(                                        // !(i ^ u => i' ^ u')
@@ -125,17 +115,16 @@ void reset(const char *path, const Dimspec &witness, const Dimspec &model,
         qand(                                    // i' ^ u'
           to_qcir(witness.initial),              // i'
           to_qcir(witness.universal))            // u'
-      ))};
+      )) };
   // expect exists(s) forall(s'\s): !(i ^ u => i' ^ u') = UNSAT
-  std::fill(std::begin(check.vars), std::end(check.vars), QVarType::ForAll);
-  for (auto& [model, witness] : shared)
-    check.vars[witness - 1] = QVarType::Exists;
+  for (auto l : extension)
+    check.vars[l - 1] = QVarType::ForAll;
   std::ofstream os { path };
   os << check;
 }
 
 void transition(const char *path, const Dimspec &witness, const Dimspec &model,
-           const std::vector<std::pair<int64_t, int64_t>> &shared) {
+           const std::vector<int64_t> &next_extension) {
   // check that forall(s) t ^ u0 ^ u1 ^ u0' => exist(s'\s) t' ^ u1'
   QCir check {
     qneg(                                        // !(t ^ u0 ^ u1 ^ u0' => t' ^ u1')
@@ -150,17 +139,16 @@ void transition(const char *path, const Dimspec &witness, const Dimspec &model,
         qand(                                    // t' ^ u1'
           to_qcir(witness.transition),           // t'
           qnext(to_qcir(witness.universal)))     // u1'
-      ))};
+      )) };
   // expect exists(s) forall(s'\s) !(t ^ u0 ^ u1 ^ u0' => t' ^ u1') = UNSAT
-  std::fill(std::begin(check.vars), std::end(check.vars), QVarType::ForAll);
-  for (auto& [model, witness] : shared)
-    check.vars[witness - 1] = QVarType::Exists;
+  for (auto l : next_extension)
+    check.vars[l - 1] = QVarType::ForAll;
   std::ofstream os { path };
   os << check;
 }
 
 void property(const char *path, const Dimspec &witness, const Dimspec &model,
-           const std::vector<std::pair<int64_t, int64_t>> &shared) {
+           const std::vector<int64_t> &extension) {
   // check that forall(s) u ^ u' => exist(s'\s) (-g => -g')
   QCir check {
     qneg(                                        // !(u ^ u' => (g => g'))
@@ -171,11 +159,10 @@ void property(const char *path, const Dimspec &witness, const Dimspec &model,
         qimply(                                  // g => g'
           to_qcir(model.goal),                   // g
           to_qcir(witness.goal))                 // g'
-      ))};
+      )) };
   // expect exists(s) forall(s'\s) !(u ^ u' => (g => g')) = UNSAT
-  std::fill(std::begin(check.vars), std::end(check.vars), QVarType::ForAll);
-  for (auto& [model, witness] : shared)
-    check.vars[witness - 1] = QVarType::Exists;
+  for (auto l : extension)
+    check.vars[l - 1] = QVarType::ForAll;
   std::ofstream os { path };
   os << check;
 }
@@ -189,9 +176,8 @@ void base(const char *path, const Dimspec &witness) {
           to_qcir(witness.initial),              // i'
           to_qcir(witness.universal)),           // u'
         qneg(to_qcir(witness.goal))              // -g'
-      ))};
+      )) };
   // expect exists(s') !(i' ^ u' => -g') = UNSAT
-  std::fill(std::begin(check.vars), std::end(check.vars), QVarType::Exists);
   std::ofstream os { path };
   os << check;
 }
@@ -209,9 +195,8 @@ void step(const char *path, const Dimspec &witness) {
             to_qcir(witness.universal),          // u0'
             qnext(to_qcir(witness.universal)))), // u1'
         qneg(qnext(to_qcir(witness.goal)))       // -g1'
-      ))};
+      )) };
   // expect exists(s') !(-g0 ^ t' ^ u0' ^ u1' => -g1') = UNSAT
-  std::fill(std::begin(check.vars), std::end(check.vars), QVarType::Exists);
   std::ofstream os { path };
   os << check;
 }
@@ -221,10 +206,10 @@ int main(int argc, char **argv) {
   MSG << "Certify Model Checking Witnesses in Dimspec\n";
   MSG << VERSION " " GITID "\n";
   Dimspec model(model_path), witness(witness_path);
-  auto shared { index_consecutively(witness, model) };
-  reset(checks[0], witness, model, shared);
-  transition(checks[1], witness, model, shared);
-  property(checks[2], witness, model, shared);
+  auto [extension, next_extension] { index_consecutively(witness, model) };
+  reset(checks[0], witness, model, extension);
+  transition(checks[1], witness, model, next_extension);
+  property(checks[2], witness, model, extension);
   base(checks[3], witness);
   step(checks[4], witness);
 }
